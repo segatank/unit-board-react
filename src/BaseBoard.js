849f4f0b8427686ac4ee3d1211a81c4ba63e4480
Maxwell <in.hate.i.trust@gmail.com>
2017-11-26 20:32:09 +0200

    added base component, editor testing

---
+++ src/BaseBoard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './BaseBoard.css';
+
+class BaseBoard extends Component {
+  render() {
+    return (
+      <div className="BaseBoard">
+      test element
+      </div>
+    );
+  }
+}
+
+export default BaseBoard;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import BaseBoard from './BaseBoard';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<BaseBoard />, document.getElementById('board-base'));
 registerServiceWorker();
