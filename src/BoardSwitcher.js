6614b87b289d1cf0d18d964dab5fd9c6455983e8
Maxwell <in.hate.i.trust@gmail.com>
2017-11-27 23:22:22 +0200

    added base for mode toggler

---
+++ src/BoardSwitcher.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './BoardSwitcher.css';
+import Checkbox from './Checkbox'
+const togglerItems = [
+  'Move',
+  'Edit'
+];
+
+
+class BoardSwitcher extends Component {
+
+  componentWillMount = () => {
+    this.selectedCheckboxes = new Set();
+  }
+
+  toggleCheckbox = label => {
+    if (this.selectedCheckboxes.has(label)) {
+      this.selectedCheckboxes.delete(label);
+    } else {
+      this.selectedCheckboxes.add(label);
+    }
+  }
+
+  createCheckbox = label => (
+    <Checkbox
+      label={label}
+      handleCheckboxChange={this.toggleCheckbox}
+      key={label}
+    />
+  )
+
+  createCheckboxes = () => (
+    togglerItems.map(this.createCheckbox)
+  )
+
+  handleFormSubmit = formSubmitEvent => {
+    formSubmitEvent.preventDefault();
+
+    for (const checkbox of this.selectedCheckboxes) {
+      console.log(checkbox, ' is selected.');
+    }
+  }
+
+  render() {
+    return (
+      <div className="BoardSwitcher">
+        <form onSubmit={this.handleFormSubmit}>
+          {this.createCheckboxes()}
+          <button className="btn btn-default" type="submit">Click</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default BoardSwitcher;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import BaseBoard from './BaseBoard';
+import BoardSwitcher from './BoardSwitcher';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
 ReactDOM.render(<BaseBoard />, document.getElementById('board-base'));
+ReactDOM.render(<BoardSwitcher />, document.getElementById('board-item-switch-mode'));
+
 registerServiceWorker();
